7c7de78487bb83e9b81a2fc8751ae8aaea2c6749
Tim <tim.bathgate@gmail.com>
2020-09-09 10:23:28 +1200

    Init

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+We are going to train a brain.js neural network to classify 
+Titanic passengers as survivors and non-survivors.
+
+Don't worry that this file is just comments... we'll be working 
+on this together.
+*/
+
+
+// 1. Configure the Neural Network
+
+// 2. Prepare the data
+
+// 3. Train the network
+
+// 4. Test the network
+
+// 5. Review the results
